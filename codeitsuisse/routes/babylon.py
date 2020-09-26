3c5627be1a1240170538bb4cdc88491edbc7a194
Jervis <jerv0003@e.ntu.edu.sg>
2020-09-26 21:40:43 +0800

    babylon and revisit

--- codeitsuisse/routes/babylon.py
+++ codeitsuisse/routes/babylon.py
@@ -1,25 +1,14 @@
 import logging
 import json
-
+import numpy as np
 from flask import request, jsonify;
 
 from codeitsuisse import app;
 
-import pip
-
 logger = logging.getLogger(__name__)
 
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-
-install('mknapsack')
-from mknapsack.algorithms import mtm
-
 @app.route('/olympiad-of-babylon', methods=['POST'])
-def evaluateOptimalBooks():
+def evaluateBabylon():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
 
@@ -28,10 +17,73 @@ def evaluateOptimalBooks():
     books = data.get("books");
     days = data.get("days");
 
-    result, x, bt, glopt = mtm([1]*numberOfBooks, books, days)
+    dp = np.full([numberOfBooks] + [i + 1 for i in days], -1)
+    result = maxWeight(days, books, numberOfBooks)
+
     # result = get_books(books, days, 0, 0, numberOfBooks)
     logging.info("My result :{}".format(result))
     return json.dumps(result);
+
+# def get_books(books, days, book_index, books_read, book_length):
+#     if book_index == book_length:
+#         return books_read
+#     book = books[book_index]
+#
+#     res = books_read
+#     can_add = False
+#
+#     for i in range(len(days)):
+#         if book <= days[i]:
+#             days[i] -= book # assume the book gets included in the day
+#             res_chosen = get_books(books, days, book_index+1, books_read+1, book_length) # recurse with the above assumption
+#             res = max(res, res_chosen)
+#             days[i] += book # book not read on that day
+#             can_add = True
+#
+#     if not can_add: # can still take in at least 1 book
+#         return res
+#
+#     res_not_chosen = get_books(books, days, book_index+1, books_read, book_length)
+#     res = max(res, res_not_chosen)
+#     return res
+
+def maxWeight(days, arr, n, i=0):
+    """
+    :param n: number of books
+    :param w1_r: capacity of bag1
+    :param w2_r: capacity of bag2
+    :param i: index of the book we are working on
+    :return:
+    """
+    # Base case
+    if i == n:
+        return 0
+    if eval('dp' + str([i]+days)) != -1:
+        return eval('dp' + str([i]+days))
+
+    # Variables to store the result of numberOfDays+1 parts of recurrence relation
+    fill_none = 0
+    fill = [0]
+    for index, d in enumerate(days):
+        if d >= arr[i]:
+            days[index] -= arr[i]
+            fill.append(1 + maxWeight(days, arr, n, i+1))
+            days[index] += arr[i]
+
+    fill_none = (1 + maxWeight(days, arr, n, i+1))
+    exec('dp' + str([i]+days) + '=' + str(max(fill_none, max(fill))))
+
+    return eval('dp' + str([i]+days))
+
+# import numpy as np
+# numberOfBooks= 5
+# numberOfDays= 3
+# books= [114, 111, 41, 62, 64]
+# days= [157, 136, 130]
+# dp = np.full([numberOfBooks] + [i+1 for i in days], -1)
+# print(maxWeight(days, books, numberOfBooks))
+# print(get_books(books, days, 0, 0, numberOfBooks))
+
 
 
 
--- codeitsuisse/routes/revisit_geometry.py
+++ codeitsuisse/routes/revisit_geometry.py
@@ -7,26 +7,65 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-# https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines/20679579
-def line(p1, p2):
-    A = (p1[1] - p2[1])
-    B = (p2[0] - p1[0])
-    C = (p1[0]*p2[1] - p2[0]*p1[1])
-    return A, B, -C
+def get_line(x1,x2,y1,y2):
+    m = (y1 - y2) / (x1 - x2)
+    c = y1 - m * x1
+    return (m,c)
 
-def intersection(L1, L2):
-    D  = L1[0] * L2[1] - L1[1] * L2[0]
-    Dx = L1[2] * L2[1] - L1[1] * L2[2]
-    Dy = L1[0] * L2[2] - L1[2] * L2[0]
-    if D != 0:
-        x = Dx / D
-        y = Dy / D
-        return x,y
+def check_valid(min_maxes, x_, y_):
+    min_x1, max_x1, min_y1, max_y1 = min_maxes
+    if min_x1 <= x_ <= max_x1 and min_y1 <= y_ <= max_y1:
+        return True
     else:
+        return False
+
+
+def line_intersection(line1, line2): # line1 = ([x1,y1], [x2,y2]), line2 = ([x3,y3], [x4,y4])
+    x1,y1 = line1[0]
+    x2,y2 = line1[1]
+    x3,y3 = line2[0]
+    x4,y4 = line2[1]
+    min_maxes = [min(x1,x2), max(x1,x2), min(y1,y2), max(y1,y2)]
+    # Both vertical lines
+    if x1 == x2 and x3 == x4:
         return None
+    # Line 1 is vertical
+    elif x1 == x2:
+        m2, c2 = get_line(x3,x4,y3,y4)
+        y_ = m2 * x1 + c2
+        # intersection = (x1, y_)
+        if check_valid(min_maxes, x1, y_):
+            return [x1,y_]
+        else:
+            return None
+    # Line 2 is vertical
+    elif x3 == x4:
+        m1, c1 = get_line(x1,x2,y1,y2)
+        y_ = m1 * x3 + c1
+        # intersection = (x3, y_)
+        if check_valid(min_maxes, x3, y_):
+            return [x3,y_]
+        else:
+            return None
+    # No vertical lines
+    else:
+        m1, c1 = get_line(x1,x2,y1,y2)
+        m2, c2 = get_line(x3,x4,y3,y4)
+
+        if m1 == m2:
+            return None
+        x_ = (c2 - c1) / (m1 - m2)
+        y1 = m1 * x_ + c1
+        y2 = m2 * x_ + c2
+
+        if check_valid(min_maxes, x_, y1):
+            return [x_, y1]
+        else:
+            return None
+
 
 @app.route('/revisitgeometry', methods=['POST'])
-def evaluateIntersections():
+def evaluate():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
 
@@ -34,19 +73,19 @@ def evaluateIntersections():
     lineCoordinates = data.get("lineCoordinates")
 
     n = len(shapeCoordinates)
-    l = len(lineCoordinates)
 
     res = []
     for i in range(n):
         second_index = (i+1)%n
-        line1 = line([shapeCoordinates[i]['x'], shapeCoordinates[i]['y']], [shapeCoordinates[second_index]['x'], shapeCoordinates[second_index]['y']])
-        line2 = line([lineCoordinates[0]['x'], lineCoordinates[0]['y']], [lineCoordinates[1]['x'], lineCoordinates[1]['y']])
-        point = intersection(line1, line2)
+        line1 = [[shapeCoordinates[i]['x'], shapeCoordinates[i]['y']], [shapeCoordinates[second_index]['x'], shapeCoordinates[second_index]['y']]]
+        line2 = [[lineCoordinates[0]['x'], lineCoordinates[0]['y']], [lineCoordinates[1]['x'], lineCoordinates[1]['y']]]
+        intersection = line_intersection(line1, line2)
+        if intersection != None:
+            res.append({"x": intersection[0], "y": intersection[1]})
 
-        if point != None and max(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) >= point[0] and point[0] >= min(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) and \
-            max(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']) >= point[1] and point[1] >= min(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']):
-            res.append({"x": point[0], "y": point[1]})
 
     logging.info("My result :{}".format(res))
     return json.dumps(res);
+
+
 
