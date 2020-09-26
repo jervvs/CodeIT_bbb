b20e66abeeea09dfab71f07e223d8fa52feb56cd
Jervis <jerv0003@e.ntu.edu.sg>
2020-09-26 13:56:51 +0800

    Update clean_floor.py

--- codeitsuisse/routes/clean_floor.py
+++ codeitsuisse/routes/clean_floor.py
@@ -15,10 +15,10 @@ def clean(arr):
     prev_one = 0 if arr[0] == 1 else -1
 
     while total != 0 and index < len(arr):
-        print(index, arr[index], prev_one)
         if arr[index] == 1:
             arr[index] = 0
-            res += 1
+            if index != 1:
+                res += 1
             total -= 1
             index += 1
         else:
@@ -30,7 +30,6 @@ def clean(arr):
                 for i in range(prev_one, index):
                     arr[i] = 0
                     res += 2
-                    print(arr, res)
 
                 arr[index] = 0
                 index += 1
@@ -38,23 +37,31 @@ def clean(arr):
             else:
                 arr[index] = 1
                 total += 1
-                res += 1
+                if index != 1:
+                    res += 1
                 prev_one = index
                 index += 1
-        print(arr, res)
+
+    if prev_one != -1:
+        for i in range(prev_one, index-1):
+            arr[i] = 0
+            res += 1
     return res
 
 @app.route('/clean_floor', methods=['POST'])
-def evaluate_clean_floor():
+def clean_floor():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
 
-    answers = {}
+    res = {'answers': {
+
+                    }
+            }
 
     inputValue = data.get("tests");
     for key, value in inputValue.items():
-        res = clean(value['floor'])
-        answers[key] = res
+        val = clean(value['floor'])
+        res['answers']['key'] = val
 
     logging.info("My result :{}".format(answers))
     return json.dumps(answers);
