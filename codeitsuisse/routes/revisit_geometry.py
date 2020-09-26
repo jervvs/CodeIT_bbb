28b2d2f2ae7d1442156de73110d2d19e3b29af7a
Jervis <jerv0003@e.ntu.edu.sg>
2020-09-26 20:30:41 +0800

    Update revisit_geometry.py

--- codeitsuisse/routes/revisit_geometry.py
+++ codeitsuisse/routes/revisit_geometry.py
@@ -8,28 +8,25 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 # https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines/20679579
-def line_intersection(line1, line2): # line1 = ([x1,y1], [x2,y2]), line2 = ([x3,y3], [x4,y4])
-    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+def line(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    return A, B, -C
 
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-       return None
-
-    d = (det(*line1), det(*line2))
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-
-    if min(line1[0][0], line1[1][0]) <= x and x <= max(line1[0][0], line1[1][0]) and min(line1[0][1], line1[1][1]) <= y and y <= max(line1[0][1], line1[1][1]):
-        return [x,y]
+def intersection(L1, L2):
+    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        x = Dx / D
+        y = Dy / D
+        return x,y
     else:
         return None
 
 @app.route('/revisitgeometry', methods=['POST'])
-def evaluate():
+def evaluateIntersections():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
 
@@ -37,19 +34,45 @@ def evaluate():
     lineCoordinates = data.get("lineCoordinates")
 
     n = len(shapeCoordinates)
+    l = len(lineCoordinates)
 
     res = []
     for i in range(n):
         second_index = (i+1)%n
-        line1 = [[shapeCoordinates[i]['x'], shapeCoordinates[i]['y']], [shapeCoordinates[second_index]['x'], shapeCoordinates[second_index]['y']]]
-        line2 = [[lineCoordinates[0]['x'], lineCoordinates[0]['y']], [lineCoordinates[1]['x'], lineCoordinates[1]['y']]]
-        intersection = line_intersection(line1, line2)
-        if intersection != None:
-            res.append({"x": intersection[0], "y": intersection[1]})
+        line1 = line([shapeCoordinates[i]['x'], shapeCoordinates[i]['y']], [shapeCoordinates[second_index]['x'], shapeCoordinates[second_index]['y']])
+        line2 = line([lineCoordinates[0]['x'], lineCoordinates[0]['y']], [lineCoordinates[1]['x'], lineCoordinates[1]['y']])
+        point = intersection(line1, line2)
 
+        if point != None and max(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) <= point[0] and point[0] >= min(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) and \
+            max(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']) <= point[1] and point[1] >= min(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']):
+            res.append({"x": intersection[0], "y": intersection[1]})
 
     logging.info("My result :{}".format(res))
     return json.dumps(res);
 
+# shapeCoordinates = [
+#     { "x": 21, "y": 70 },
+#     { "x": 72, "y": 70 },
+#     { "x": 72, "y": 127 }
+#   ]
+# lineCoordinates = [
+#     { "x": -58, "y": 56 },
+#     { "x": -28, "y": 68 }
+#   ]
+#
+# n = len(shapeCoordinates)
+#
+# res = []
+# for i in range(n):
+#     second_index = (i+1)%n
+#     line1 = line([shapeCoordinates[i]['x'], shapeCoordinates[i]['y']], [shapeCoordinates[second_index]['x'], shapeCoordinates[second_index]['y']])
+#     line2 = line([lineCoordinates[0]['x'], lineCoordinates[0]['y']], [lineCoordinates[1]['x'], lineCoordinates[1]['y']])
+#     point = intersection(line1, line2)
+#
+#     if point != None and max(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) >= point[0] and point[0] >= min(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) and \
+#         max(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']) >= point[1] and point[1] >= min(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']):
+#         res.append({"x": point[0], "y": point[1]})
+#
+# print(res)
 
 
