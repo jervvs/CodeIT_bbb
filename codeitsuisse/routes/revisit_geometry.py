15e2e84514c2321702821dc5a354f583e11cce33
Jervis <jerv0003@e.ntu.edu.sg>
2020-09-26 20:37:25 +0800

    Update revisit_geometry.py

--- codeitsuisse/routes/revisit_geometry.py
+++ codeitsuisse/routes/revisit_geometry.py
@@ -43,36 +43,10 @@ def evaluateIntersections():
         line2 = line([lineCoordinates[0]['x'], lineCoordinates[0]['y']], [lineCoordinates[1]['x'], lineCoordinates[1]['y']])
         point = intersection(line1, line2)
 
-        if point != None and max(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) <= point[0] and point[0] >= min(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) and \
-            max(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']) <= point[1] and point[1] >= min(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']):
-            res.append({"x": intersection[0], "y": intersection[1]})
+        if point != None and max(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) >= point[0] and point[0] >= min(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) and \
+            max(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']) >= point[1] and point[1] >= min(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']):
+            res.append({"x": point[0], "y": point[1]})
 
     logging.info("My result :{}".format(res))
     return json.dumps(res);
-
-# shapeCoordinates = [
-#     { "x": 21, "y": 70 },
-#     { "x": 72, "y": 70 },
-#     { "x": 72, "y": 127 }
-#   ]
-# lineCoordinates = [
-#     { "x": -58, "y": 56 },
-#     { "x": -28, "y": 68 }
-#   ]
-#
-# n = len(shapeCoordinates)
-#
-# res = []
-# for i in range(n):
-#     second_index = (i+1)%n
-#     line1 = line([shapeCoordinates[i]['x'], shapeCoordinates[i]['y']], [shapeCoordinates[second_index]['x'], shapeCoordinates[second_index]['y']])
-#     line2 = line([lineCoordinates[0]['x'], lineCoordinates[0]['y']], [lineCoordinates[1]['x'], lineCoordinates[1]['y']])
-#     point = intersection(line1, line2)
-#
-#     if point != None and max(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) >= point[0] and point[0] >= min(shapeCoordinates[i]['x'], shapeCoordinates[second_index]['x']) and \
-#         max(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']) >= point[1] and point[1] >= min(shapeCoordinates[i]['y'], shapeCoordinates[second_index]['y']):
-#         res.append({"x": point[0], "y": point[1]})
-#
-# print(res)
-
 
