81c606395afd169604debfca513cb35340aa5ac1
Jervis <jerv0003@e.ntu.edu.sg>
2020-09-26 13:56:26 +0800

    Update salad_spree.py

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -14,20 +14,21 @@ def evaluateSalad():
     n = data.get('number_of_salads')
     arr = data.get("salad_prices_street_map")
 
-    res = 10**9 + 7
+    res = 10 ** 9 + 7
     rows = len(arr)
     cols = len(arr[0])
     for i in range(rows):
         tmp = arr[i]
         tmp = [int(i) if i != 'X' else 0 for i in tmp]
-        for j in range(cols-n+1):
-            if 0 not in tmp[j:j+n]:
-                res = min(res, sum(tmp[j+n]))
+        for j in range(cols - n + 1):
+            if 0 not in tmp[j:j + n]:
+                res = min(res, sum(tmp[j:j + n]))
 
-    if res != 10^9 + 7:
+    if res == 10 ** 9 + 7:
         res = 0
 
-    logging.info("My result :{}".format(res))
+    result = {'result': res}
+    logging.info("My result :{}".format(result))
     return json.dumps(res);
 
 
