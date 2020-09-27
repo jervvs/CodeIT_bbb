4cfd2c87d84b2694e7f979e4105b9767fe3679fd
Jervis <jerv0003@e.ntu.edu.sg>
2020-09-27 09:34:26 +0800

    supermarket

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -12,6 +12,8 @@ import codeitsuisse.routes.cluster
 import codeitsuisse.routes.hedge
 import codeitsuisse.routes.contact_trace
 import codeitsuisse.routes.inventory_management
+import codeitsuisse.routes.supermarket
+
 
 
 
